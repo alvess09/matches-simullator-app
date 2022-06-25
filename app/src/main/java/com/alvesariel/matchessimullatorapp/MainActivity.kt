9a18d70f0cc1287008dff97d5eb4da77dd0bd485
Ariel Alves <arielalves9@gmail.com>
2022-06-25 11:46:17 -0300

    Add ViewBinding

--- app/src/main/java/com/alvesariel/matchessimullatorapp/MainActivity.kt
+++ app/src/main/java/com/alvesariel/matchessimullatorapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.alvesariel.matchessimullatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alvesariel.matchessimullatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.tvHello.text = "teste"
     }
 }
