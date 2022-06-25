49c7e1b219fa484d51ee7d7bc4c3d17743e38417
Ariel Alves <arielalves9@gmail.com>
2022-06-25 11:37:00 -0300

    my first commit

---
+++ app/src/main/java/com/alvesariel/matchessimullatorapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alvesariel.matchessimullatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
