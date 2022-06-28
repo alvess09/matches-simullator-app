45f339a84a2d3947573bd446d48798b59553e450
Ariel Alves <arielalves9@gmail.com>
2022-06-27 21:00:35 -0300

    add Detail Activity

---
+++ app/src/main/java/com/alvesariel/matchessimullatorapp/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.alvesariel.matchessimullatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alvesariel.matchessimullatorapp.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    //criamos o binding
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //aqui dizemos que ele recebe activityDetailBinding concatenando com inflatelayout
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
