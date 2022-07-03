f2fdf1473d4bce54fb5f937475c512d43b1ac1bb
Ariel Alves <arielalves9@gmail.com>
2022-07-02 23:12:01 -0300

    Update README, and add function in DetailActivity

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# MATCHES SIMULATOR APP
+# MATCHES SIMULATOR APP 🎲
 ### Project developed during Bootcamp developer mobile at Santander
 
-> 🧑🏻‍💻Santander  && Digital Innovation One 🧑🏻‍💻
+> 🧑🏻‍💻Santander && Digital Innovation One 🧑🏻‍💻
 
 ## This Project contain 🚀
 
@@ -9,16 +9,33 @@ IN DEVELOPMENT! ..
 ## Frontend
 [X] 1 Develop native from Android apps  </br>
 [X] 2 Components, layouts and UI/UX in Android apps </br>
+[X] 3 Development Activities screens with  </br>
 ## Backend
 [X] 1 Consuming API with Retrofit and Glide</br>
 [X] 2 Creating API in GitHub Pages </br>
-[X] 3 </br>
-[X] 6</br>
-[X] 7</br>
+> https://github.com/alvess09/matches-simulator-api
+
+[X] 3 Using Parcelize with Parcelable </br>
+[X] 6 Development using Java and Kotlin to this App</br>
+
+## Prototype
 
 ![GitHub Logo](/prototypeOne.jpg)
 
+## Final App
+
+![GitHub Logo](/finalApp.jpg)
 
 > Teacher: Venilton F JR. </br>
 > https://github.com/digitalinnovationone
+
+
+> Documentation to the libraries
+> Retrofit 
+> https://github.com/square/retrofit
+> Glide
+> https://github.com/bumptech/glide
+> Parcelable
+> https://developer.android.com/kotlin/parcelize?hl=pt-br
+
 
--- app/src/main/java/com/alvesariel/matchessimullatorapp/ui/DetailActivity.kt
+++ app/src/main/java/com/alvesariel/matchessimullatorapp/ui/DetailActivity.kt
@@ -3,6 +3,8 @@ package com.alvesariel.matchessimullatorapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alvesariel.matchessimullatorapp.databinding.ActivityDetailBinding
+import com.alvesariel.matchessimullatorapp.domain.Match
+import com.bumptech.glide.Glide
 
 class DetailActivity : AppCompatActivity() {
     //criamos o binding
@@ -14,11 +16,40 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         //aqui dizemos que ele recebe activityDetailBinding concatenando com inflatelayout
+
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
+        loadMatchFromExtra()
+
+    }
+
+    private fun loadMatchFromExtra() {
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let{
+            Glide.with(this).load(it.place.image).into(binding.ivPlace)
+            supportActionBar?.title = it.place.name
+            binding.tvDescription.text = it.description
+
+            // carregar infos dos times casa
+            Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            if(it.homeTeam.score != null){
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+            // carregar infos dos times  fora
+            Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            if(it.homeTeam.score != null){
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
+        }
     }
 }
